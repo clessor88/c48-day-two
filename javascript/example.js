b464994f049d22f7bbdde978ccecf7a41c8da30e
Crystal Lessor <clessor88@yahoo.com>
2015-10-28 21:02:09 -0700

    first commit

---
+++ javascript/example.js
@@ -0,0 +1,15 @@
+alert("it worked!");
+console.log("I'm hungry!")
+
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+var aLocation = "university of washington";
+var quote = 'A great man once said "always do such and such"'
+console.log(someNumber);
+console.log(anotherNumber);
+console.log(quote);
+
+var favoriteFoods =['pizza', 'icecream','burgers'];
+console.log(favoriteFoods);
+console.log(favoriteFoods[0]);
+var thirdFav = favoritefoods[2];
